3a9e669ba95cd3a38d0b779459cbb05250e6da2d
emmett555505 <edonaldson1122@gmail.com>
2025-07-28 14:34:53 -0400

    project 12

--- project12.py
+++ project12.py
@@ -4,7 +4,7 @@ t.Screen().setup(300,400)
 polygon = t.Turtle() #defined variable
 
 num_sides = 4 #variable
-side_lengths = 70
+side_lengths = 50
 angle = 360.0 / num_sides
 #iterate loop for total nubmer of sides
 for i in range(num_sides):
